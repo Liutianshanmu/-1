d6e079e98b700ec9c07a9a983ff50c0f07f29302
Liutianshanmu <499748593@qq.com>
2022-05-12 16:18:13 +0800

    master

---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module Èí¼ş¹¤³Ì1 {
+	requires java.desktop;
+}
---
+++ src/软件工程1/main.java
@@ -0,0 +1,7 @@
+package Èí¼þ¹¤³Ì1;
+
+public class main {
+    public static void main(String args[]) {
+    	Controll controll=new Controll();
+    }
+}
